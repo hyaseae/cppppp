306a5fdc0326104b730a1c9c38640be29ce3d883
telescope0901 h <s220073@unjung.hs.kr>
2024-07-31 05:58:39 +0000

    으아아아아아아ㅏㅇㅇ

--- cpp/2096.cpp
+++ cpp/2096.cpp
@@ -1,16 +1,24 @@
 #include <stdio.h>
-int min, max, n, i;
-short nums[100000][3];
+int min,max,j, n, i, a, b, c;
+short nums[100001][3];
+short difs[100001][3];
 // 뭐야 이거 어카지
 int main()
 {
+    int min_now = 0;
+    int max_now = 0;
+    int max_now_going = 1; //-1 0 1로 하면 햇갈ㄹ게지? 0 1 2
+    int min_now_going = 1;
     scanf("%d", &n);
-    for (i = 0; i < n; i++)
+    for (i = 1; i <= n; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (j = 0; j < 3; j++)
         {
             scanf("%hd", &nums[i][j]);
         }
+        //idea 1 <- 연속성 보장을 못함
+        //3진 트리<- 뭔 도른짓이지 이건...
+        //최댓값만 두번 돌면 최소도 구해짐. 따라서 최대/최소만 구할줄 알면 됨
     }
 
     return 0;
