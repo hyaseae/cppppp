f80ca63cd4f7f49c3d5bcfbe7a70778258eadd94
telescope0901 h <s220073@unjung.hs.kr>
2024-08-12 02:45:51 +0000

    ㅁㄴㅇㄹㅁㄴㅇㄹ

--- cpp/2096.cpp
+++ cpp/2096.cpp
@@ -16,9 +16,21 @@ int main()
         {
             scanf("%hd", &nums[i][j]);
         }
+        if (i==1) continue;
+        int lookat = i-1;
+        difs[lookat][0] = nums[lookat][0]-nums[lookat][1]+
         //idea 1 <- 연속성 보장을 못함
         //3진 트리<- 뭔 도른짓이지 이건...
         //최댓값만 두번 돌면 최소도 구해짐. 따라서 최대/최소만 구할줄 알면 됨
+        /*
+        100 
+        100
+        100
+        100
+        100
+        009
+        0 0 0 0 1 2//idea 1 맞을지도?
+        */
     }
 
     return 0;
