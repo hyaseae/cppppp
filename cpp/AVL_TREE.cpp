82bd48f5f09874cf3ceba392a2de7f1757a3eda6
telescope0901 h <s220073@unjung.hs.kr>
2024-08-27 11:37:27 +0000

    dtd =

--- cpp/AVL_TREE.cpp
+++ cpp/AVL_TREE.cpp
@@ -1,10 +1,28 @@
 #include <stdio.h>
-int a[100000];
-void what()
+typedef struct nodedata {
+    int value;
+    int parent;
+    int c1,c2;
+    int height = 0;
+    short BF = 0;
+} node;
+node a[100000];
+int end = 1;
+void swap_node_for_print(int a1, int a2)
 {
-    for (auto i :a)
+    node t = a[a1];
+    a[a1] = a[a2];
+    a[a2] = t;
+}
+void print_node()
+{
+    if (end == -1)
     {
-        printf("%d ",i);
+        return;
+    }
+    for (int i = 0; i < end; i ++)
+    {
+        printf("%d %d",a[i].value,a[i].BF);
     }
 }
 int main()
@@ -13,6 +31,6 @@ int main()
     scanf("%d",&n);
     for (int i = 0 ; i < n; i ++)
     {
-        scanf("%d",&a[i]);
+        scanf("%d",&a[i].value);
     }
 }
