02e0425ee34847fd57dbf3a908a19e1cda61eb6b
telescope0901 h <s220073@unjung.hs.kr>
2024-09-21 13:31:55 +0000

    ㅁㄴㅇㄹ

--- cpp/1786-kmp.cpp
+++ cpp/1786-kmp.cpp
@@ -2,13 +2,40 @@
 int k[1000000];
 char T[1000000];
 char P[1000000];
-int calc_kmp()
+void calc_kmp()
 {
-    return 1;
+    k[0] = 0;
+    int i = 0;
+    int j = 1;
+    while (P[j] != '\0')
+    {
+        if (P[i] == P[j])
+        {
+            i++;
+            if ((j + 1) < (i + 1) * 2)
+            {
+                i--;
+            }
+            k[j] = k[j - 1] + 1;
+            printf("%d %d %d\n", P[i],i,j);
+        }
+        else
+        {
+            i = 0;
+        }
+        j++;
+    }
+    i = 0;
+    while (P[i] != '\0')
+    {
+        printf("%d ", k[i]);
+        i++;
+    }
 }
 int main()
 {
-    scanf("%s\n",T);
-    scanf("%s",P);
+    scanf("%s\n", T); // 원본
+    scanf("%s", P);   // 찾는거
+    calc_kmp();
     return 1;
 }
